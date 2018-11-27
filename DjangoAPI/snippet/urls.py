e8446c5cdaf316164de54e717905cbf213122936
Ryan Febriansyah <37971350+sodrooome@users.noreply.github.com>
2018-11-27 12:38:32 +0700

    added url api_root

--- DjangoAPI/snippet/urls.py
+++ DjangoAPI/snippet/urls.py
@@ -4,10 +4,11 @@ from snippet import views
 
 urlpatterns = [
 
-    url(r'^snippet/', views.SnippetList.as_view()),
-    url(r'^snippet/<int:pk>/', views.SnippetDetail.as_view()),
-    url(r'^user/', views.UserList.as_view()),
-    url(r'^user/<int:pk>/', views.UserDetail.as_view()),
+    url(r'^snippet/', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippet/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    url(r'^user/', views.UserList.as_view(), name='user-list'),
+    url(r'^user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    url(r'', views.api_root_point),
 
 ]
 
