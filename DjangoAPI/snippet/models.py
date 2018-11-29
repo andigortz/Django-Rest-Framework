47356dbe2bf188c2692405d506fffb09be672103
Ryan Febriansyah <37971350+sodrooome@users.noreply.github.com>
2018-11-29 15:55:54 +0700

    remove update in class snippet

--- DjangoAPI/snippet/models.py
+++ DjangoAPI/snippet/models.py
@@ -11,10 +11,9 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
-    title = models.CharField(max_length=255, blank=True, unique=True, null=False, default='')
+    title = models.CharField(max_length=255, blank=True, default='')
     code = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=150)
