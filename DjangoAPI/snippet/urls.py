76cc6f8f21be03766341f81e765145cfb5547acd
Ryan Febriansyah <37971350+sodrooome@users.noreply.github.com>
2018-11-29 15:33:06 +0700

    added schema

--- DjangoAPI/snippet/urls.py
+++ DjangoAPI/snippet/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
 from snippet import views
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
 
+    url(r'^schema/', schema_view),
     url(r'^snippet/', views.SnippetList.as_view(), name='snippet-list'),
     url(r'^snippet/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
     url(r'^snippet/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
