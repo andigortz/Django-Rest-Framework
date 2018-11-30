c9d08549c057ace8a94cf66c18f73dd05cceaa61
Ryan Febriansyah <37971350+sodrooome@users.noreply.github.com>
2018-11-30 19:07:41 +0700

    rechange url to path

--- DjangoAPI/snippet/urls.py
+++ DjangoAPI/snippet/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.schemas import get_schema_view
 from snippet import views
@@ -7,13 +7,13 @@ schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
 
-    url(r'^schema/', schema_view),
-    url(r'^snippet/', views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippet/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippet/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^user/', views.UserList.as_view(), name='user-list'),
-    url(r'^user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    url(r'', views.api_root),
+    path('schema/', schema_view),
+    path('snippet/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippet/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippet/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('user/', views.UserList.as_view(), name='user-list'),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root),
 
 ]
 
